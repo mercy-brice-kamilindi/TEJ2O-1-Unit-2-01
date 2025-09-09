b1fb74102e4e6e1338d12e9f8e40fcc6a12cfa04
mercy-brice-kamilindi <mercy.brice.kamilindi@stu.ocsb.ca>
2025-09-09 17:29:35 +0000

    added python version

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,10 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Brice
+Created on: Sep 2025
 This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.scroll("Brice")
